e6febaa4ddbf4e1ba10650b2a38281aac37e424f
chepsi <evanschepsiror@gmail.com>
2023-08-23 20:52:46 +0300

    Fix: Offline first.

---
+++ core/data/src/main/java/chepsi/weather/data/home/mappers/DatabaseToDataModelMapper.kt
@@ -0,0 +1,40 @@
+package chepsi.weather.data.home.mappers
+
+import chepsi.weather.data.home.model.CityCoordinatesDataModel
+import chepsi.weather.data.home.model.CityForecastDataModel
+import chepsi.weather.data.home.model.ForecastDataModel
+import chepsi.weather.local_data_source.city.model.CityEntity
+import chepsi.weather.local_data_source.weather.model.ForecastEntity
+import chepsi.weather.local_data_source.weather.model.WeatherLocalSourceEntity
+
+object DatabaseToDataModelMapper {
+
+    fun toData(
+        weather: WeatherLocalSourceEntity,
+        city: CityEntity,
+        forecast: List<ForecastEntity>
+    ) = ForecastDataModel(
+        cityName = city.cityName,
+        cityId = city.cityId,
+        cityForecast = forecast.toData(),
+        coordinates = CityCoordinatesDataModel(
+            latitude = city.latitude,
+            longitude = city.longitude
+        ),
+        seaLevel = weather.seaLevel,
+        weather = weather.weather,
+        minimumTemperature = weather.minimumTemperature,
+        maximumTemperature = weather.maximumTemperature,
+        currentTemperature = weather.temperature,
+    )
+
+    fun List<ForecastEntity>.toData() = map {
+        CityForecastDataModel(
+            date = it.date,
+            temperature = it.temperature,
+            minimumTemperature = it.minimumTemperature,
+            maximumTemperature = it.maximumTemperature,
+            weather = it.weather
+        )
+    }
+}
--- core/data/src/main/java/chepsi/weather/data/home/repository/HomeDataRepository.kt
+++ core/data/src/main/java/chepsi/weather/data/home/repository/HomeDataRepository.kt
@@ -1,24 +1,26 @@
 package chepsi.weather.data.home.repository
 
 import chepsi.weather.data.home.mappers.ApiToDataModelMapper.toData
+import chepsi.weather.data.home.mappers.DataToDatabaseMapper.toCityEntity
+import chepsi.weather.data.home.mappers.DataToDatabaseMapper.toForecastEntity
+import chepsi.weather.data.home.mappers.DataToDatabaseMapper.toWeatherEntity
 import chepsi.weather.data.home.mappers.DataToDomainModelMapper.toDomain
+import chepsi.weather.data.home.mappers.DatabaseToDataModelMapper
 import chepsi.weather.data.home.model.ForecastDataModel
 import chepsi.weather.domain.home.model.HomeRepositoryDomainModel
 import chepsi.weather.domain.home.repository.HomeRepository
 import chepsi.weather.local_data_source.city.dao.CityDao
-import chepsi.weather.local_data_source.city.model.CityEntity
 import chepsi.weather.local_data_source.location.LocationSource
 import chepsi.weather.local_data_source.weather.dao.ForecastDao
 import chepsi.weather.local_data_source.weather.dao.WeatherDao
-import chepsi.weather.local_data_source.weather.model.ForecastEntity
-import chepsi.weather.local_data_source.weather.model.WeatherLocalSourceEntity
 import chepsi.weather.remote_data_source.api.WeatherRemoteSource
 import chepsi.weather.remote_data_source.models.LocationRequestModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flowOf
+import timber.log.Timber
 
 class HomeDataRepository @Inject constructor(
     private val weatherRemoteSource: WeatherRemoteSource,
@@ -27,53 +29,32 @@ class HomeDataRepository @Inject constructor(
     private val forecastDao: ForecastDao,
     private val cityDao: CityDao
 ) : HomeRepository {
-    override suspend fun fetchHomeInformation(): Flow<HomeRepositoryDomainModel> {
+    override suspend fun fetchHomeInformation(): Flow<HomeRepositoryDomainModel> = combine(
+        flow = weatherDao.getAll(),
+        flow2 = cityDao.getAll(),
+        flow3 = forecastDao.getAll()
+    ) { weather, city, forecast ->
+         if (forecast.isEmpty()) {
+            refreshDatabase().toDomain()
+        } else {
+            DatabaseToDataModelMapper.toData(weather, city, forecast).toDomain()
+        }
+    }.catch { exception ->
+        Timber.e(exception)
+    }
+
+    private suspend fun refreshDatabase(): ForecastDataModel {
         val currentLocation = locationSource.fetchCurrentLocation().first()
         val locationRequestModel = LocationRequestModel(
             latitude = currentLocation.latitude,
             longitude = currentLocation.longitude
         )
-        return combine(
-            flow = flowOf(weatherRemoteSource.fetchCurrentLocationWeather(locationRequestModel)),
-            flow2 = flowOf(weatherRemoteSource.fetchDaysAheadWeather(locationRequestModel))
-        ) { currentWeatherApiModel, forecastApiModel ->
-            val dataModel = currentWeatherApiModel.toData(forecastApiModel)
-            refreshDatabase(dataModel)
-            dataModel.toDomain()
-        }
-    }
-
-    private suspend fun refreshDatabase(dataModel: ForecastDataModel){
-        val weatherEntity = WeatherLocalSourceEntity(
-            cityId = dataModel.cityId,
-            cityName = dataModel.cityName,
-            seaLevel = dataModel.seaLevel,
-            createdAt = System.currentTimeMillis(),
-            maximumTemperature = dataModel.maximumTemperature,
-            minimumTemperature = dataModel.minimumTemperature,
-            temperature = dataModel.currentTemperature,
-            weather = dataModel.weather
-        )
-        val cityEntity = CityEntity(
-            cityId = dataModel.cityId,
-            cityName = dataModel.cityName,
-            isCurrent = true,
-            latitude = dataModel.coordinates.latitude,
-            longitude = dataModel.coordinates.longitude
-        )
-        val forecastEntity = dataModel.cityForecast.map {
-            ForecastEntity(
-                minimumTemperature = it.minimumTemperature,
-                maximumTemperature = it.maximumTemperature,
-                temperature = it.temperature,
-                date = it.date,
-                weather = it.weather,
-                cityId = dataModel.cityId
-            )
-        }
-
-        cityDao.insert(cityEntity)
-        forecastDao.insert(forecastEntity)
-        weatherDao.insert(weatherEntity)
+        val weather = weatherRemoteSource.fetchCurrentLocationWeather(locationRequestModel)
+        val forecast = weatherRemoteSource.fetchDaysAheadWeather(locationRequestModel)
+        val data = weather.toData(forecast)
+        weatherDao.insert(data.toWeatherEntity())
+        forecastDao.insert(data.toForecastEntity())
+        cityDao.insert(data.toCityEntity())
+        return data
     }
 }
