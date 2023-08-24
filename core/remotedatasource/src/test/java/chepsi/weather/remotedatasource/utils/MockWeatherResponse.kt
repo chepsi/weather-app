32c5ab0f6ab85235e1bb46327cb640211e1491e0
chepsi <evanschepsiror@gmail.com>
2023-08-24 17:36:48 +0300

    Fix: Lint

--- core/remotedatasource/src/test/java/chepsi/weather/remotedatasource/utils/MockWeatherResponse.kt
+++ core/remotedatasource/src/test/java/chepsi/weather/remotedatasource/utils/MockWeatherResponse.kt
@@ -1,45 +1,45 @@
 package chepsi.weather.remotedatasource.utils
 
 val mockWeatherResponse = "{\n" +
-        "    \"coord\": {\n" +
-        "        \"lon\": 36.8219,\n" +
-        "        \"lat\": -1.2921\n" +
-        "    },\n" +
-        "    \"weather\": [\n" +
-        "        {\n" +
-        "            \"id\": 802,\n" +
-        "            \"main\": \"Clouds\",\n" +
-        "            \"description\": \"scattered clouds\",\n" +
-        "            \"icon\": \"03d\"\n" +
-        "        }\n" +
-        "    ],\n" +
-        "    \"base\": \"stations\",\n" +
-        "    \"main\": {\n" +
-        "        \"temp\": 25.29,\n" +
-        "        \"feels_like\": 24.81,\n" +
-        "        \"temp_min\": 22.86,\n" +
-        "        \"temp_max\": 25.71,\n" +
-        "        \"pressure\": 1021,\n" +
-        "        \"humidity\": 36\n" +
-        "    },\n" +
-        "    \"visibility\": 10000,\n" +
-        "    \"wind\": {\n" +
-        "        \"speed\": 5.66,\n" +
-        "        \"deg\": 70\n" +
-        "    },\n" +
-        "    \"clouds\": {\n" +
-        "        \"all\": 40\n" +
-        "    },\n" +
-        "    \"dt\": 1692884357,\n" +
-        "    \"sys\": {\n" +
-        "        \"type\": 1,\n" +
-        "        \"id\": 2558,\n" +
-        "        \"country\": \"KE\",\n" +
-        "        \"sunrise\": 1692847984,\n" +
-        "        \"sunset\": 1692891467\n" +
-        "    },\n" +
-        "    \"timezone\": 10800,\n" +
-        "    \"id\": 184745,\n" +
-        "    \"name\": \"Nairobi\",\n" +
-        "    \"cod\": 200\n" +
-        "}"
+    "    \"coord\": {\n" +
+    "        \"lon\": 36.8219,\n" +
+    "        \"lat\": -1.2921\n" +
+    "    },\n" +
+    "    \"weather\": [\n" +
+    "        {\n" +
+    "            \"id\": 802,\n" +
+    "            \"main\": \"Clouds\",\n" +
+    "            \"description\": \"scattered clouds\",\n" +
+    "            \"icon\": \"03d\"\n" +
+    "        }\n" +
+    "    ],\n" +
+    "    \"base\": \"stations\",\n" +
+    "    \"main\": {\n" +
+    "        \"temp\": 25.29,\n" +
+    "        \"feels_like\": 24.81,\n" +
+    "        \"temp_min\": 22.86,\n" +
+    "        \"temp_max\": 25.71,\n" +
+    "        \"pressure\": 1021,\n" +
+    "        \"humidity\": 36\n" +
+    "    },\n" +
+    "    \"visibility\": 10000,\n" +
+    "    \"wind\": {\n" +
+    "        \"speed\": 5.66,\n" +
+    "        \"deg\": 70\n" +
+    "    },\n" +
+    "    \"clouds\": {\n" +
+    "        \"all\": 40\n" +
+    "    },\n" +
+    "    \"dt\": 1692884357,\n" +
+    "    \"sys\": {\n" +
+    "        \"type\": 1,\n" +
+    "        \"id\": 2558,\n" +
+    "        \"country\": \"KE\",\n" +
+    "        \"sunrise\": 1692847984,\n" +
+    "        \"sunset\": 1692891467\n" +
+    "    },\n" +
+    "    \"timezone\": 10800,\n" +
+    "    \"id\": 184745,\n" +
+    "    \"name\": \"Nairobi\",\n" +
+    "    \"cod\": 200\n" +
+    "}"
